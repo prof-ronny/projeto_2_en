5f62c90b2713ac6e2646fdb015697f1d460708cc
prof-ronny <carlos.ronny@gmail.com>
2026-02-17 12:32:42 -0300

    step 2

--- chapter_2_4.dart
+++ chapter_2_4.dart
@@ -1,3 +1,11 @@
 void main() {
   print('Welcome to the Simple Dart Calculator!');
+
+  double num1 = 10; // Enter the first number
+  double num2 = 5; // Enter the second number
+  String operation = '+'; // choose the operation
+
+  print('Number 1: $num1');
+  print('Number 2: $num2');
+  print('Chosen operation: $operation');
 }
