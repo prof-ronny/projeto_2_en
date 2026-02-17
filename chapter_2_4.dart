b4f09f0599617b53a86f1ce9aee6c46185aef328
prof-ronny <carlos.ronny@gmail.com>
2026-02-17 12:35:13 -0300

    step 3

--- chapter_2_4.dart
+++ chapter_2_4.dart
@@ -1,11 +1,31 @@
 void main() {
   print('Welcome to the Simple Dart Calculator!');
 
-  double num1 = 10; // Enter the first number
-  double num2 = 5; // Enter the second number
-  String operation = '+'; // choose the operation
+  double num1 = 10;
+  double num2 = 5;
+  String operation = '+'; // Change to -, *, or /
 
-  print('Number 1: $num1');
-  print('Number 2: $num2');
-  print('Chosen operation: $operation');
+  double result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num2 != 0
+          ? num1 / num2
+          : double.nan;
+      break;
+    default:
+      print('Invalid operation');
+      return;
+  }
+
+  print('The result is: $result');
 }
